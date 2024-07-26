6471ef15d34c795ab0b8b06ff555c71bd4972049
Jashany <jsingh12_be22@thapar.edu>
2024-07-26 18:22:27 +0530

    appointemnt

--- src/Pages/Booking/Booking.jsx
+++ src/Pages/Booking/Booking.jsx
@@ -26,13 +26,12 @@ const offerrId = atom("");
 const Booking = () => {
   const navigate = useNavigate();
   const user = useSelector((state) => state.auth.auth);
-  
 
   const [loading, setLoading] = useState(true);
 
   const [coupon, setCoupon] = useState("");
   const [offer, setOffer] = useState("");
-  const [offerId, setOfferId] = useState(offerrId);
+  const [offerId, setOfferId] = useAtom(offerrId);
   const [showCoupon, setShowCoupon] = useState(false);
   const [data, setData] = useState({});
   const [discount, setDiscount] = useAtom(discountAmount);
@@ -42,14 +41,14 @@ const Booking = () => {
   const services = service?.map((service) => service.id);
   const appointment = useSelector((state) => state.appointment.appointment);
   const [isButtonDisabled, setIsButtonDisabled] = useState(false);
-  const [showoffer, setShowOffer] = useAtom(couponAtom)
+  const [showoffer, setShowOffer] = useAtom(couponAtom);
   const [appliedCoupon, setAppliedCoupon] = useAtom(code);
 
   useEffect(() => {
     if (!user) {
       window.location.href = "/";
     }
-    
+
     setLoading(true);
 
     fetch("https://api.salondekho.in/api/appointment/getCost", {
@@ -88,7 +87,6 @@ const Booking = () => {
     );
   }
 
- 
   const googleMapsUrl = `https://www.google.com/maps/dir/?api=1&destination=${data?.salon?.location?.coordinates[0]},${data?.salon?.location?.coordinates[1]}`;
 
   //2024-06-17 get day of week
@@ -112,24 +110,26 @@ const Booking = () => {
   appointmentEndTime.setMinutes(appointmentStartTime.getMinutes() + duration);
   const hours = appointmentEndTime.getHours();
   let minutes = appointmentEndTime.getMinutes();
-  
+
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-  
-  if(showoffer){
-    return(
-      <OfferPage salonId={salonid} day={dayOfWeek} date={appointment.appointmentDate} />
-    )
+
+  if (showoffer) {
+    return (
+      <OfferPage
+        salonId={salonid}
+        day={dayOfWeek}
+        date={appointment.appointmentDate}
+      />
+    );
   }
-
-
- 
 
   const appointmentStart = `${appointment.appointmentDate}T${appointment.appointmentStartTime}:00.000`;
 
   const submitBooking = () => {
     // Disable the button for 3 seconds after the first click
+    console.log(offerId);
     setIsButtonDisabled(true);
     setTimeout(() => {
       setIsButtonDisabled(false);
@@ -155,11 +155,14 @@ const Booking = () => {
       .then((response) => response.json())
       .then((data) => {
         if (data.success === true) {
-          navigate(`/success/${data?.data}?redirect=/appointment/${data?.data}`, {
-            state: {
-              text: "Booking Confirmed",
-            },
-          });
+          navigate(
+            `/success/${data?.data}?redirect=/appointment/${data?.data}`,
+            {
+              state: {
+                text: "Booking Confirmed",
+              },
+            }
+          );
         } else {
           toast.error(data?.message);
         }
@@ -183,12 +186,16 @@ const Booking = () => {
     data?.salon?.Reviews.reduce((total, review) => total + review.Rating, 0) /
       data?.salon?.Reviews.length || 0;
 
-    const ratings = averageRating % 1 === 0 ? averageRating : averageRating.toFixed(1);
+  const ratings =
+    averageRating % 1 === 0 ? averageRating : averageRating.toFixed(1);
 
   return (
     <div className={styles.main}>
-      <div style={{marginBottom:"3.5rem"}}>
-        <Header text={"Review and Confirm"} redirect={`/salon/${salonid}/${appointment.artistId}`} />
+      <div style={{ marginBottom: "3.5rem" }}>
+        <Header
+          text={"Review and Confirm"}
+          redirect={`/salon/${salonid}/${appointment.artistId}`}
+        />
 
         <div className={styles.salon}>
           <div>
@@ -257,7 +264,8 @@ const Booking = () => {
           </h3>
           <h3>
             <img src={clock} />
-            {ConvertTime(appointment.appointmentStartTime)} - {ConvertTime(`${hours}:${minutes}`)}
+            {ConvertTime(appointment.appointmentStartTime)} -{" "}
+            {ConvertTime(`${hours}:${minutes}`)}
           </h3>
           <h3>
             <BiUser />
@@ -275,17 +283,21 @@ const Booking = () => {
               disabled
             />
             {appliedCoupon ? (
-            <button onClick={()=>{
-              setDiscount(0)
-              setCoupon("")
-              setOffer("")
-              setAppliedCoupon("")
-            }}>{`x`}</button>
-          ):(
-            <button onClick={()=>{
-              setShowOffer(true)
-            }}>{`>`}</button>
-          )}
+              <button
+                onClick={() => {
+                  setDiscount(0);
+                  setCoupon("");
+                  setOffer("");
+                  setAppliedCoupon("");
+                }}
+              >{`x`}</button>
+            ) : (
+              <button
+                onClick={() => {
+                  setShowOffer(true);
+                }}
+              >{`>`}</button>
+            )}
           </div>
         </div>
         <div className={styles.services}>
@@ -302,8 +314,6 @@ const Booking = () => {
             </div>
           ))}
         </div>
-
-      
 
         <div className={styles.payment}>
           <div>
@@ -333,30 +343,28 @@ const Booking = () => {
         </div>
       </div>
       <div className={styles.confirmBox}>
-      <button
-        className={styles.confirm}
-        disabled={isButtonDisabled}
-        onClick={submitBooking}
+        <button
+          className={styles.confirm}
+          disabled={isButtonDisabled}
+          onClick={submitBooking}
         >
-        Confirm 
-      </button>
-        </div>
+          Confirm
+        </button>
+      </div>
     </div>
   );
 };
 
-
-const OfferPage = ({salonId,day,date}) =>{
-  const [offer, setOffer] = useAtom(code)
-  const [offerId, setOfferId] = useAtom(offerrId)
-  const [showoffer, setShowOffer] = useAtom(couponAtom)
-  console.log(salonId,day)
-  const [offers,setOffers] = useState([])
+const OfferPage = ({ salonId, day, date }) => {
+  const [offer, setOffer] = useAtom(code);
+  const [offerId, setOfferId] = useAtom(offerrId);
+  const [showoffer, setShowOffer] = useAtom(couponAtom);
+  console.log(salonId, day);
+  const [offers, setOffers] = useState([]);
   const [discount, setDiscount] = useAtom(discountAmount);
 
-
   const SubmitCoupon = (offer) => {
-    console.log(offer)
+    console.log(offer);
     if (!offer) {
       toast.error("Please enter a coupon code");
       return;
@@ -378,10 +386,10 @@ const OfferPage = ({salonId,day,date}) =>{
         if (data.success == true) {
           setDiscount(data?.data);
           setShowOffer(false);
+          console.log(data.offerId);
           setOfferId(data?.offerId);
           toast.success(data?.message);
           setOffer(offer);
-          setCoupon("");
         } else {
           toast.error(data?.message);
         }
@@ -391,59 +399,70 @@ const OfferPage = ({salonId,day,date}) =>{
       });
   };
 
-
-  useEffect(()=>{
-    fetch("https://api.salondekho.in/api/offer/get-offers-of-that-day",{
-      method:"POST",
-      credentials:"include",
-      headers:{
-        "Content-Type":"application/json"
+  useEffect(() => {
+    fetch("https://api.salondekho.in/api/offer/get-offers-of-that-day", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
       },
-      body:JSON.stringify({
+      body: JSON.stringify({
         salonId,
-        day
+        day,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setOffers(data?.data);
       })
-    }).then(response=>response.json())
-    .then(data=>{
-      console.log(data)
-      setOffers(data?.data)
-    })
-    .catch(error=>{
-      console.log(error)
-    })
-  },[])
-  return(
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  return (
     <div className={styles.offers}>
       <div className={styles.header}>
-        <img src={backArrow} alt="" onClick={()=>{
-          console.log("clicked")
-          setShowOffer(false)
-        }} />
-        <h3 style={{marginRight:"20px"}}>OFFERS</h3>
+        <img
+          src={backArrow}
+          alt=""
+          onClick={() => {
+            console.log("clicked");
+            setShowOffer(false);
+          }}
+        />
+        <h3 style={{ marginRight: "20px" }}>OFFERS</h3>
         <p></p>
       </div>
       <div className={styles.offerss}>
-        {offers.map(offer=>(
+        {offers.map((offer) => (
           <div className={styles.offer}>
             <div>
-            <div style={{display:"flex",justifyContent:"space-between",alignItems:"center"}}>
-            <h4>Flat {offer.OfferDiscountinPercentage}% off</h4>
-            <Ticket text={offer?.OfferName} />
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                }}
+              >
+                <h4>Flat {offer.OfferDiscountinPercentage}% off</h4>
+                <Ticket text={offer?.OfferName} />
+              </div>
+              <p>{offer.OfferDescription}</p>
             </div>
-            <p>{offer.OfferDescription}</p>
-            </div>
-            <div className={styles.apply} onClick={()=>{
-              SubmitCoupon(offer.OfferName)
-            }}>
-              <h6>
-                TAP TO APPLY
-              </h6>
+            <div
+              className={styles.apply}
+              onClick={() => {
+                SubmitCoupon(offer.OfferName);
+              }}
+            >
+              <h6>TAP TO APPLY</h6>
             </div>
           </div>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Booking;
